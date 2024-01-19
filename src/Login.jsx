c84b7e3272c92da0a02ef12e3fa87b46889786c8
Adarsh Mishra <141004812+switchbreaks@users.noreply.github.com>
2024-01-19 22:45:55 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,54 +1,60 @@
-import React  from "react";
-import './login.css'
-import { Link } from "react-router-dom";
-
-//      login page code replace with fee pay mater 
-//              this is not login page code only name is login.jsx
+// this is not  login file login code inside admin file
 
 
-    const Login =()=>{
-        return (
-            <>
-              <div className="super_class">
-                <div className="mainclass">
-                <h1 className="topclassh1"> Pay your fee and fill form</h1>
-                <img src="qr.jpeg" alt="img not found" className="img_about_class"/>
-                <div className="login_input">
-                        <div className="inputbox" name="class">
-                                            <label>Pay Your Fee</label>
-                                            <select required>
-                                                <option disabled selected>Select Your Class</option>
-                                                <option >6 pay 300 ₹/Month</option>
-                                                <option>7 Pay 400 ₹/Month</option>
-                                                <option>8 pay 450 ₹/Month</option>
-                                                <option>9 pay 500 ₹/Month</option>
-                                                <option>11 pay 550 ₹/Month</option>
-                                                <option>Exam fee 550 ₹</option>
-                                            </select>
-                                        </div>
-                                        </div>
-                <form className="login" action="https://formspree.io/f/xvollgvk" method="POST">
 
-                    <div className="login_input">
-                        <input type="Text" placeholder="Enter Roll Number" name="rollno" required/>
-                    </div>
-                    <div className="login_input">
-                        <input type="Text" placeholder="Student Name" name="name" required/>
-                    </div>
-                    <div className="login_input">
-                     <input type= "text" placeholder="Enter translation ID" name="Translation_Id" required/>
-                     </div>
-                     <div className="login_input">
-                     <input type="submit" value="Submit" className="btm_login" required/>
-                     </div>
-                     <div className="login_input">
-                     <input type="reset" value="Reset" className="btm_login" required/>
-                     </div>
-                </form>
-                     <Link to="/" className="link">If Any Query! &nbsp; Fill Query Form</Link>
-                </div>
-              </div>
-            </>
-        );
-    }
-    export default Login;
+
+
+// import React  from "react";
+// import './login.css'
+// import { Link } from "react-router-dom";
+
+// //      login page code replace with fee pay mater 
+// //              this is not login page code only name is login.jsx
+
+
+//     const Login =()=>{
+//         return (
+//             <>
+//               <div className="super_class">
+//                 <div className="mainclass">
+//                 <h1 className="topclassh1"> Pay your fee and fill form</h1>
+//                 <img src="qr.jpeg" alt="img not found" className="img_about_class"/>
+//                 <div className="login_input">
+//                         <div className="inputbox" name="class">
+//                                             <label>Pay Your Fee</label>
+//                                             <select required>
+//                                                 <option disabled selected>Select Your Class</option>
+//                                                 <option >6 pay 300 ₹/Month</option>
+//                                                 <option>7 Pay 400 ₹/Month</option>
+//                                                 <option>8 pay 450 ₹/Month</option>
+//                                                 <option>9 pay 500 ₹/Month</option>
+//                                                 <option>11 pay 550 ₹/Month</option>
+//                                                 <option>Exam fee 550 ₹</option>
+//                                             </select>
+//                                         </div>
+//                                         </div>
+//                 <form className="login" action="https://formspree.io/f/xvollgvk" method="POST">
+
+//                     <div className="login_input">
+//                         <input type="Text" placeholder="Enter Roll Number" name="rollno" required/>
+//                     </div>
+//                     <div className="login_input">
+//                         <input type="Text" placeholder="Student Name" name="name" required/>
+//                     </div>
+//                     <div className="login_input">
+//                      <input type= "text" placeholder="Enter translation ID" name="Translation_Id" required/>
+//                      </div>
+//                      <div className="login_input">
+//                      <input type="submit" value="Submit" className="btm_login" required/>
+//                      </div>
+//                      <div className="login_input">
+//                      <input type="reset" value="Reset" className="btm_login" required/>
+//                      </div>
+//                 </form>
+//                      <Link to="/" className="link">If Any Query! &nbsp; Fill Query Form</Link>
+//                 </div>
+//               </div>
+//             </>
+//         );
+//     }
+//     export default Login;
